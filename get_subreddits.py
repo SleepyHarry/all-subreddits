8fb0be16d8d4cf3bf6d0b31ad9ead3ad43c12478
Harry Vane <harryavane@gmail.com>
2015-04-24 19:44:21 +0100

    Initial commit

---
+++ get_subreddits.py
@@ -0,0 +1,28 @@
+import re
+import requests
+
+url = 'http://redditmetrics.com/history/'
+
+class ConnectionError(Exception):
+    pass
+
+response = requests.get(url)
+
+if not response.ok:
+    raise ConnectionError
+else:
+    dates = set(re.findall(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", response.text))
+
+def get_subs_from_text(text):
+    return set(re.findall(r"/r/[a-z0-9-.]+", text, re.IGNORECASE))
+
+def get_page_from_date(date):
+    response = requests.get(url + date)
+    
+    if not response.ok:
+        raise ConnectionError
+    
+    return response.text
+
+def get_subs_from_date(date):
+    return get_subs_from_page(get_page_from_date(date))
